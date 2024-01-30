83265e2b7650ed71a40c2dd16b93e607e6ee7e05
james4510 <james451055@gmail.com>
2024-01-30 12:32:22 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+#AWAKERS
+
+## Vue.js 개발환경 설정 
+1. 해당 리포지토리 .zip파일 다운
+2. zip파일 바탕화면에 압축 해제
+3. VScode에서 해당 폴더로 이동
+   1. cd `desktop`
+   2. cd `2024-AWAKERS-develop`
+   3. cd `2024-AWAKERS-develop`
+   4. npm install
+5. npm run dev로 프로젝트 실행
+6. `npm run dev` : 개발자 모드로 실행 <br>
+
+## 중요 명령어
+- `npm run dev` : 개발자 모드로 실행
+- `Ctrl + c` : 작업 중지<br>
+
+
+## Visual Studio Code Extensions
+- `Veter`
+- `ES7 React snippets`
+- `ESLint`
+- `VSCode Great Icons`
+- `Prettier-Code formatter`
+- `Visual Studio IntelliCode`
+- `HTML CSS Support` : HTML 요소의 class에서 css 선택자 요소를 쓸 때 자동 완성 기능 지원해주는 익스텐션
+- `HTML to CSS autocompletion` : CSS에서 코딩할 때 HTML에서 사용한 속성이름을 자동 완성해주는 기능 지원해주는 익스텐션
+- `Auto Rename Tag` : 태그 이름을 수정할 때, <>여는 태그와 </>닫는 태그를 동시에 수정하게 해주는 익스텐션
+- `Auto Close Tag` : 태그를 작성할 때 여는 태그만 입력하면 닫는 태그가 자동으로 작성이 되게 하는 익스텐션
+- `Highlight Matching Tag` : 쌍이 되는 태그를 표시해 주는 익스텐션
+- `HTML End Tag Labels` : 마지막 닫는 태그에 클래스명을 주석처리로 표시하여 이 태그가 어느 역할을 하는 태그인지 알 수 있게 해주는 익스텐션
+- `HTMLHing` : html 코딩할 때 알맞지 않은 문법이나 빠진 부분이 있으면 ide처럼 알려주는 익스텐션
+- `image preview` : 이미지의 프리뷰를 볼 수 있게 하는 익스텐션
+- `HTML Auto Completion` : 태그 구성 자동 완성 익스텐션
+    1. !html 치고 엔터
+    2. !table 치고 엔터
+    3. !form 치고 엔터
+- `Stylelint` : css 구문 오류를 잡아주게 도와주는 익스텐션
+- `vscode-styled-components` : css문법 표현 자동완성 기능 제공하는 익스텐션
---
+++ src/components/LoginView.vue
@@ -0,0 +1,121 @@
+<template>
+<div class="LoginView">
+    <div>
+        <!-- 로그인 문자열 -->
+        <div class="loginHeader">
+            <p class="pageTitle">로그인</p>
+        </div>
+        
+        <!-- 어웨이커스 로고 -->
+        <img src="../assets/어웨이커스.svg" alt="AWAKERS LOGO" class="loginImage" />
+        
+        <div class="inputData">
+
+            <!-- 이름 입력란 -->
+            <input type="text" id="name" placeholder="이름" class="bottomBorder" v-model="name"/>
+            <br>
+            <!-- 전화번호 입력란 -->
+            <input type="text" id="phone" placeholder="연락처" class="bottomBorder" v-model="phoneNumber" />
+        
+        </div>
+
+    </div>
+        
+    <footer>
+        <!-- 로그인 버튼 -->
+        <button type="button" onclick="location.href='#/AttendancePage'">로그인 하기</button>
+    </footer>
+</div>
+    
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            name: "",
+            phoneNumber: "",
+        };
+    },
+    methods: {
+        login(){
+            //로그인 로직
+            console.log("로그인 시도:", this.name, this.phoneNumber);
+            //여기에 실제 로그인 로직 추가하기
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/scss/main.scss";
+
+.LoginView{
+    .loginHeader{
+        text-align: center;
+        font-size: 15px;
+        margin-top: 20px;
+        display: flex;
+
+        .pageTitle{
+            height: 100%;
+            width: 95%;
+        }
+    
+    }
+
+    .loginImage{
+        display: block;
+        margin: 50px auto;
+        max-width: 100%;
+        height: auto;
+    }
+
+    label{
+        display: block;
+        margin-top: 10px;
+    }
+
+    .inputData{
+        line-height: 60px;
+            input{
+            width: 320px;
+            padding: 5px;
+            margin-bottom: 10px;
+        }
+
+        input:focus{
+            outline: none;
+        }
+    }
+
+    .bottomBorder{
+        border-width: 0 0 1px;
+    }
+
+    footer{
+        position: absolute;
+        bottom: 20px;
+    
+        button{
+            @include center;
+            font-size: 15px;
+            font-weight: bold;
+            width: 330px;
+            padding: 15px;
+            background-color: #606761;
+            color: white;
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+            bottom: 0;
+        }
+
+        button:hover{
+            background-color: #171a17;
+        }
+    }
+    
+}
+
+</style>
