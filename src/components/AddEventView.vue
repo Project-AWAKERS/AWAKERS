40c1d8dae92ad2057380509b826faf80fe76100a
james4510 <james451055@gmail.com>
2024-01-30 14:45:02 +0900

    feat: 출석관리 페이지 작업함
    출석관리 페이지 완성
    
    1

--- src/components/AddEventView.vue
+++ src/components/AddEventView.vue
@@ -3,13 +3,6 @@
     <div class="AddEventView">
         <div class="header">
 
-            <!-- 뒤로 가기 버튼 -->
-            <div class="closeButton">
-                <a href="javascript:history.back()" class="title"><img src="../assets/close.svg" alt="닫기"/></a>
-            </div>
-            
-            <!-- 페이지 제목-->
-            <p class="pageTitle">출석관리</p>
         </div>
     </div>
   </body>
